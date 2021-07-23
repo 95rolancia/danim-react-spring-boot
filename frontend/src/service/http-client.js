61c3ed6f41defcb40bb3cb6a17c92e553d8b03ba
rolancia <95jjangjun@gmail.com>
2021-07-23 11:31:03 +0900

    feat:[#125] Add signup in HttpClient

--- frontend/src/service/http-client.js
+++ frontend/src/service/http-client.js
@@ -7,4 +7,13 @@ export class HttpClient {
     const res = await this.instance.post('/signin', user);
     return res;
   }
+
+  async signUp(user) {
+    try {
+      const res = await this.instance.post('/signup', user);
+      return res;
+    } catch (error) {
+      throw new Error(`sign up error ${error}`);
+    }
+  }
 }
