ee2c0e83aab9a429fd4a662d490707f165d0fab9
이종현 <dlwhdgus53@naver.com>
2021-07-23 10:38:37 +0900

    Create SpringBoot Application

---
+++ backend/danim-back/src/main/java/com/pd/danim/DanimBackApplication.java
@@ -0,0 +1,13 @@
+package com.pd.danim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanimBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanimBackApplication.class, args);
+	}
+
+}
---
+++ backend/danim-back/src/test/java/com/pd/danim/DanimBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pd.danim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanimBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
