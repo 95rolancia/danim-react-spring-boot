69283bd134d2c3768baa04283d2faadc1390e949
노영주 <at8in@naver.com>
2021-07-23 18:22:37 +0900

    refactor: fix input-validator

--- frontend/src/util/input-validator.js
+++ frontend/src/util/input-validator.js
@@ -1,27 +1,28 @@
 export class InputValidator {
-     checkPassword = (pwd) => {
-        const regExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,10}$/
-        if (regExp.test(pwd)) {
-            return true;
-        } 
-            return false;
-        
-    }
+    checkPassword = (pwd) => {
+       const regExp = /^[a-zA-Z0-9]{8,12}$/
+       if (regExp.test(pwd)) {
+           return true;
+       } 
+           return false;
+       
+   }
 
-     checkEmail = (email) => {
-        const regExp =/[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]$/i;
-        if (regExp.test(email)) {
-            return true;
-        } 
-            return false;
-    }
+    checkEmail = (email) => {
+       const regExp =/[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]$/i;
+       if (regExp.test(email)) {
+           return true;
+       } 
+           return false;
+   }
 
-     checkNickname = (nickname) => {
-        const regExp = /^[가-힣]{2,12}|[a-zA-Z]{2,12}\s[a-zA-Z]{2,15}$/;
-        if (regExp.test(nickname)) {
-            return true;
-        } 
-            return false;
-    }
+    checkNickname = (nickname) => {
+        const regExp = /^([a-zA-Z0-9ㄱ-ㅎ|ㅏ-ㅣ|가-힣]).{1,11}$/;
+
+       if (regExp.test(nickname)) {
+           return true;
+       } 
+           return false;
+   }
 }
- 
+
