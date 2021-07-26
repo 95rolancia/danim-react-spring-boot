cb90e29df33a2f905450760a803003998ca374d8
muni <ansgml6491@naver.com>
2021-07-26 14:43:52 +0900

    feat: [#202] app.jsx에 라우터 돔 추가하고 pages에 각 페이지 만들고 연결

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,7 +1,48 @@
-import styles from './app.module.css';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import SignIn from './pages/sign-in/sign-in'
+import SignUp from './pages/sign-up/sign-up'
+import Main from './pages/main/main'
+import Search from './pages/search/search'
+import Trip from './pages/trip/trip'
+import Bookmark from './pages/bookmark/bookmark'
+import Profile from './pages/profile/profile'
+// import styles from './app.module.css';
 
 function App() {
-  return <h1 className={styles.title}>Hello world</h1>;
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/main">Main</Link>
+        <Link to="/search">Search</Link>
+        <Link to="/trip">Trip</Link>
+        <Link to="/bookmark">Bookmark</Link>
+        <Link to="/my">My</Link>
+      </nav>
+      <Switch>
+        <Route path={['/', '/signin']} exact>
+          <SignIn />
+        </Route>
+        <Route path="/signup" exact>
+          <SignUp />
+        </Route>
+        <Route path="/main" exact>
+          <Main />
+        </Route>
+        <Route path="/search" exact>
+          <Search />
+        </Route>
+        <Route path="/trip" exact>
+          <Trip />
+        </Route>
+        <Route path="/bookmark" exact>
+          <Bookmark />
+        </Route>
+        <Route path="/profile" exact>
+          <Profile />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/bookmark/bookmark.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Bookmark = (props) => (
+      <h1>북마크</h1>
+  );
+
+export default Bookmark;
