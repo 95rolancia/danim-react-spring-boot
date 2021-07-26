90cdd05582034a15c94d4635beefbc728165113b
muni <ansgml6491@naver.com>
2021-07-26 16:35:17 +0900

    refactor: [#202] app.jsx 에 있던 라우터 main.jsx로 분리

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,42 +1,8 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import SignIn from './pages/sign-in/sign-in'
-import SignUp from './pages/sign-up/sign-up'
-import Main from './pages/main/main'
-import Search from './pages/search/search'
-import Trip from './pages/trip/trip'
-import Bookmark from './pages/bookmark/bookmark'
-import Profile from './pages/profile/profile'
-import Navbar from './components/navbar/navbar';
 // import styles from './app.module.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path={['/', '/signin']} exact>
-          <SignIn />
-        </Route>
-        <Route path="/signup" exact>
-          <SignUp />
-        </Route>
-        <Route path="/main" exact>
-          <Main />
-        </Route>
-        <Route path="/search" exact>
-          <Search />
-        </Route>
-        <Route path="/trip" exact>
-          <Trip />
-        </Route>
-        <Route path="/bookmark" exact>
-          <Bookmark />
-        </Route>
-        <Route path="/profile" exact>
-          <Profile />
-        </Route>
-      </Switch>
-      <Navbar />
-    </BrowserRouter>
+    <h1>app</h1>
   );
 }
 
--- frontend/src/pages/main/main.jsx
+++ frontend/src/pages/main/main.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Search from './pages/search/search'
+import Trip from './pages/trip/trip'
+import Bookmark from './pages/bookmark/bookmark'
+import Profile from './pages/profile/profile'
+import Navbar from './components/navbar/navbar';
 
-const Main = (props) => (
-      <h1>main</h1>
+const Main = (props) => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+        
+        </Route>
+        <Route path="/search" exact>
+          <Search />
+        </Route>
+        <Route path="/trip" exact>
+          <Trip />
+        </Route>
+        <Route path="/bookmark" exact>
+          <Bookmark />
+        </Route>
+        <Route path="/profile" exact>
+          <Profile />
+        </Route>
+      </Switch>
+      <Navbar />
+    </BrowserRouter>
   );
+}
 
 export default Main;
