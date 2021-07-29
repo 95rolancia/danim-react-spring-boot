011f0d0f7184b6a4cbb819b74f969fc341c70607
노영주 <at8in@naver.com>
2021-07-29 19:27:10 +0900

    refactor:[#213]Header logout button추가

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -1,22 +1,41 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import { makeStyles, Typography } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import ExploreIcon from '@material-ui/icons/Explore';
+import { Button, IconButton, makeStyles, Typography } from '@material-ui/core';
+import { Link, useHistory } from 'react-router-dom';
 
-// const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
 const Header = (props) => {
-  // const classes = useStyles();
+  const classes = useStyles();
+  const history = useHistory();
+  const moveHome = () => {
+    history.push('/');
+  };
   return (
-    <AppBar>
-      <Toolbar>
-        <Typography variant="button">
-          <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>
-            DANIM
-          </Link>
-        </Typography>
-      </Toolbar>
-    </AppBar>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="logo">
+            <ExploreIcon onClick={moveHome} />
+          </IconButton>
+          <Typography variant="button" className={classes.title}>
+            <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>
+              DANIM
+            </Link>
+          </Typography>
+          <Button color="inherit">LOGOUT</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 };
 
