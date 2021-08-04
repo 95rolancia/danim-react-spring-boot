cfb1d474e3c4b52b4a83a6bf7962c080fd214af1
이종현 <dlwhdgus53@naver.com>
2021-08-04 17:24:08 +0900

    feat : [#472] set-status-jwt

--- backend/danim-back/src/main/java/com/pd/danim/Util/JwtUtil.java
+++ backend/danim-back/src/main/java/com/pd/danim/Util/JwtUtil.java
@@ -19,8 +19,8 @@ import java.util.Date;
 @Component
 public class JwtUtil {
 
-    public final static long TOKEN_VALIDATION_SECOND = 1000L * 60 * 30 ;
-    public final static long REFRESH_TOKEN_VALIDATION_SECOND = 1000L * 60 * 60 * 24;
+    public final static long TOKEN_VALIDATION_SECOND = 1000L * 10  ;
+    public final static long REFRESH_TOKEN_VALIDATION_SECOND = 1000L * 60 * 30;
 
     final static public String ACCESS_TOKEN_NAME = "accessToken";
     final static public String REFRESH_TOKEN_NAME = "refreshToken";
