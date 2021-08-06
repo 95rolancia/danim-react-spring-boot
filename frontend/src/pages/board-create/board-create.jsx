6975dc25f69e12ae446c652c8bc173628fc5a977
muni <ansgml6491@naver.com>
2021-08-06 11:11:26 +0900

    feat: [#253] gps 정보 표기

--- frontend/src/pages/board-create/board-create.jsx
+++ frontend/src/pages/board-create/board-create.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Img } from './index';
 import uuid from 'react-uuid';
 import { makeStyles, Container, Button, Grid } from '@material-ui/core';
-// import EXIF from 'exif-js';
+import loadImage from 'blueimp-load-image';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -36,32 +36,51 @@ const BoardCreate = () => {
 
   const handleClick = (img) => {
     console.log('you clicked!', img);
-    // getMetaData(img);
   };
 
-  // const getMetaData = (img) => {
-  //   console.log('getMetaData 실행');
-  //   EXIF.getData(img, () => {
-  //     console.log('getting Meta Data', img);
-  //     // let exifData = EXIF.pretty(img);
-  //     let exifLongitude = EXIF.getTag(img, 'GPSLatitude');
-  //     let exifLatitude = EXIF.getTag(img, 'GPSLongitude');
-  //     console.log('longitude', exifLongitude);
-  //     console.log('latitude', exifLatitude);
-  //   });
-  // };
+  const getMetaData = async (imgFile) => {
+    console.log('getMetaData 실행');
 
-  const handleFileChange = (e) => {
+    const result = await loadImage.parseMetaData(imgFile, {
+      maxMetaDataSize: 262144,
+    });
+
+    if (result.exif.get('GPSInfo') !== undefined) {
+      console.log('date: ', result.exif.get('Exif').get('DateTimeDigitized'));
+      console.log('gps info: ', result.exif.get('GPSInfo').getAll());
+      return {
+        dateTimeDigitized: result.exif.get('Exif').get('DateTimeDigitized'),
+        gpsInfo: result.exif.get('GPSInfo').getAll(),
+      };
+    } else {
+      return false;
+    }
+  };
+
+  const handleFileChange = async (e) => {
     for (let i = 0; i < e.target.files.length; i++) {
       if (e.target.files[i]) {
         let reader = new FileReader();
         reader.readAsDataURL(e.target.files[i]); // 파일을 읽어 버퍼에 저장
         // onload 콜백함수는 비동기로 실행되므로 콜백함수 안에서 html요소를 생성하거나 appendChild하면 안됨
-        reader.onloadend = () => {
+        reader.onloadend = async () => {
           const saveURL = reader.result;
           if (saveURL) {
             let saveURLSub = saveURL.toString();
             let imgObj = {};
+            let exifData = await getMetaData(e.target.files[i]);
+            console.log('exif 데이터 있나요', exifData);
+            if (exifData.gpsInfo !== undefined) {
+              console.log('exif 데이터 언디파인드 아님!');
+              imgObj['isExifData'] = true;
+              imgObj['dateTimeDigitized'] = exifData.dateTimeDigitized;
+              imgObj['gpsInfo'] = exifData.gpsInfo;
+            } else {
+              console.log('exif 데이터 언디파인드임!');
+              imgObj['isExifData'] = false;
+              imgObj['dateTimeDigitized'] = null;
+              imgObj['gpsInfo'] = null;
+            }
             imgObj['imgFile'] = e.target.files[i];
             imgObj['key'] = uuid();
             imgObj['imgPreviewURL'] = saveURLSub;
--- frontend/src/pages/board-create/components/img.jsx
+++ frontend/src/pages/board-create/components/img.jsx
@@ -2,6 +2,10 @@ import { makeStyles } from '@material-ui/core';
 import React from 'react';
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
   thumbnail: {
     height: '30vw',
     width: '30vw',
@@ -18,12 +22,30 @@ const Img = ({ img, onClick }) => {
   };
 
   return (
-    <img
-      src={img.imgPreviewURL}
-      alt={img.imgFile.name}
-      className={classes.thumbnail}
-      onClick={handleClick}
-    />
+    <div className={classes.root}>
+      <img
+        src={img.imgPreviewURL}
+        alt={img.imgFile.name}
+        className={classes.thumbnail}
+        onClick={handleClick}
+      />
+      <div>
+        {img.isExifData ? (
+          <div>
+            date: {img.dateTimeDigitized}
+            <br></br>
+            Altitude: {img.gpsInfo.GPSAltitude}
+            <br></br>
+            latitude: {img.gpsInfo.GPSLatitude}
+            <br></br>
+            longitude: {img.gpsInfo.GPSLongitude}
+            <br></br>
+          </div>
+        ) : (
+          <div>gps정보 없어요</div>
+        )}
+      </div>
+    </div>
   );
 };
 
