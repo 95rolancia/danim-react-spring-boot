4ec1f540f2537b953b4340daa0a0faa36d70179f
rolancia <95jjangjun@gmail.com>
2021-08-09 17:42:30 +0900

    feat: [#674] 개인 설정 컴포넌트

--- frontend/src/pages/my-page/components/index.js
+++ frontend/src/pages/my-page/components/index.js
@@ -1,3 +1,4 @@
 export { default as Header } from './header/header';
 export { default as Introduce } from './introduce/introduce';
 export { default as Profile } from './profile/profile';
+export { default as Setting } from './setting/setting';
