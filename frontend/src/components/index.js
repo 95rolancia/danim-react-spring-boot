bf8ede20427b584026d430436b23e3bf08ebf2e9
rolancia <95jjangjun@gmail.com>
2021-08-09 17:21:30 +0900

    feat: [#672] Create account-edit

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,3 +1,4 @@
+export { default as AccountEdit } from './account-edit/account-edit';
 export { default as Header } from './header/main-header';
 export { default as Navbar } from './navbar/navbar';
 export { default as StartLoading } from './start-loading/start-loading';
