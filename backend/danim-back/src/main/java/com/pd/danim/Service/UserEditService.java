331904a6a7c7f312d0b35ef0289ca26052c016b7
shining8543 <shining8543@naver.com>
2021-08-10 21:17:25 +0900

    refactor:[#691] userEdit Refactoring

--- backend/danim-back/src/main/java/com/pd/danim/Service/UserEditService.java
+++ backend/danim-back/src/main/java/com/pd/danim/Service/UserEditService.java
@@ -1,12 +1,15 @@
 package com.pd.danim.Service;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.multipart.MultipartFile;
+
 import com.pd.danim.Form.Request.PasswordRequest;
-import com.pd.danim.Form.Request.ProfileRequest;
 import com.pd.danim.Form.Request.UserEditRequest;
 
 public interface UserEditService {
 
-	String uploadProfile(ProfileRequest profileReq);
-	boolean setUserInfo(UserEditRequest userEditReq);
-	boolean setPassword(PasswordRequest pwdReq);
+	String uploadProfile(MultipartFile file, HttpServletRequest httpServletReq);
+	boolean setUserInfo(UserEditRequest userEditReq, HttpServletRequest httpServletReq);
+	boolean setPassword(PasswordRequest pwdReq, HttpServletRequest httpServletReq);
 }
