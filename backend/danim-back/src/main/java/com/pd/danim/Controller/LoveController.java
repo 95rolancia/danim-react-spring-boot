0344a80b6777fb4cd5a4330991764a18933f8ce3
이종현 <dlwhdgus53@naver.com>
2021-08-12 14:39:17 +0900

    hotfix : [#717] revise-status-code

--- backend/danim-back/src/main/java/com/pd/danim/Controller/LoveController.java
+++ backend/danim-back/src/main/java/com/pd/danim/Controller/LoveController.java
@@ -15,18 +15,21 @@ import com.pd.danim.Form.Response.LoveCheckResponse;
 import com.pd.danim.Service.LoveService;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
-@Api(tags = "팔로우", value = "follow contoller")
+@Api(tags = "좋아요", value = "love contoller")
 public class LoveController {
 
 	@Autowired
 	private LoveService loveService;
-
+	
+	@ApiOperation(tags ="좋아요 생성", value ="좋아요 수 증가 및 관계 설정")
 	@PostMapping("/love")
 	public ResponseEntity<LoveCheckResponse> addLove(@RequestBody LoveRequest loveRequest,
 			HttpServletRequest httpServletRequest) {
 
+		
 		String message = loveService.addLove(loveRequest, httpServletRequest);
 		LoveCheckResponse loveCheckResponse = new LoveCheckResponse();
 		if (message.equals("success")) {
@@ -44,7 +47,7 @@ public class LoveController {
 
 		}
 	}
-
+	@ApiOperation(tags ="좋아요 제거", value ="좋아요 수 감소 및 관계 설정")
 	@DeleteMapping("/love")
 	public ResponseEntity<LoveCheckResponse> deleteLove(@RequestBody LoveRequest loveRequest,
 			HttpServletRequest httpServletRequest) {
@@ -52,15 +55,15 @@ public class LoveController {
 		String message = loveService.deleteLove(loveRequest, httpServletRequest);
 		LoveCheckResponse loveCheckResponse = new LoveCheckResponse();
 		if (message.equals("success")) {
-			loveCheckResponse.setIsLove(true);
+			loveCheckResponse.setIsLove(false);
 			loveCheckResponse.setMessage("success");
 			return new ResponseEntity<LoveCheckResponse>(loveCheckResponse,HttpStatus.OK);
 		} else if (message.equals("doesn't exist")) {
-			loveCheckResponse.setIsLove(false);
+			loveCheckResponse.setIsLove(true);
 			loveCheckResponse.setMessage("doesn't exist");
 			return new ResponseEntity<LoveCheckResponse>(loveCheckResponse,HttpStatus.BAD_REQUEST);
 		}	else {
-			loveCheckResponse.setIsLove(false);
+			loveCheckResponse.setIsLove(true);
 			loveCheckResponse.setMessage("server error");
 			return new ResponseEntity<LoveCheckResponse>(loveCheckResponse,HttpStatus.INTERNAL_SERVER_ERROR);
 
