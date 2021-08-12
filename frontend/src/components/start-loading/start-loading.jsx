2c14b7f7b890f617e12619ab5d3dcb731d7e58ef
rolancia <95jjangjun@gmail.com>
2021-08-12 17:41:28 +0900

    refactor: start-loading component

--- frontend/src/components/start-loading/start-loading.jsx
+++ frontend/src/components/start-loading/start-loading.jsx
@@ -1,52 +1,51 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-
-const StartLoading = (props) => {
-  const classes = useStyles();
-  return (
-    <div className={classes.main}>
-      <img
-        className={classes.background}
-        width="100%"
-        height="100%"
-        src="images/main-background.gif"
-        alt=""
-      />
-      <Typography className={classes.title} variant="button">
-        여행과 추억속으로 ,
-      </Typography>
-      <Typography className={classes.title} variant="h2" component="h2">
-        DANIM
-      </Typography>
-    </div>
-  );
-};
+import { Typography, makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   main: {
     position: 'relative',
     display: 'flex',
     flexDirection: 'column',
-    alignItems: 'center',
     justifyContent: 'center',
     width: '100%',
-    height: '100vh',
-  },
-  title: {
-    color: 'whitesmoke',
-    // fontWeight: 'bold',
-    fontFamily: 'MingukBold',
-    // [theme.breakpoints.up('md')]: {
-    //   fontSize: '4rem',
-    // },
+    height: '100%',
   },
   background: {
     position: 'absolute',
     top: '0',
     opacity: '0.8',
-    zIndex: '-1',
+    width: '100%',
+    height: '100%',
+  },
+  titleContainer: {
+    textAlign: 'center',
+    paddingBottom: '5em',
+    zIndex: '3',
+  },
+  title: {
+    color: 'whitesmoke',
+    fontFamily: 'MingukBold',
   },
 }));
+
+const StartLoading = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.main}>
+      <img
+        className={classes.background}
+        src="images/main-background.gif"
+        alt="landing-page-background-img"
+      />
+      <section className={classes.titleContainer}>
+        <Typography className={classes.title}>여행과 추억속으로 ,</Typography>
+        <Typography className={classes.title} variant="h2" component="h2">
+          DANIM
+        </Typography>
+      </section>
+    </div>
+  );
+};
 
 export default StartLoading;
