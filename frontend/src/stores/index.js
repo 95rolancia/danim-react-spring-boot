78f6933a1f7a2e798cb86cccd410a52b53c8e14c
rolancia <95jjangjun@gmail.com>
2021-08-13 23:57:03 +0900

    feat: [#758] custom use search hook

--- frontend/src/stores/index.js
+++ frontend/src/stores/index.js
@@ -1,14 +1,20 @@
 import React, { createContext } from 'react';
 import authStore from './auth-store';
 import userStore from './user-store';
+import searchStore from './search-store';
 
 export const AuthContext = createContext();
 export const UserContext = createContext();
+export const SearchContext = createContext();
 
 const ProviderStores = ({ children }) => {
   return (
     <AuthContext.Provider value={authStore}>
-      <UserContext.Provider value={userStore}>{children}</UserContext.Provider>
+      <UserContext.Provider value={userStore}>
+        <SearchContext.Provider value={searchStore}>
+          {children}
+        </SearchContext.Provider>
+      </UserContext.Provider>
     </AuthContext.Provider>
   );
 };
