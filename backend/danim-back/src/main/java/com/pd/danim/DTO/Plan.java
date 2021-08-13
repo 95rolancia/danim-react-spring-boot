3244c09bbfc94449f4a56bfcb07dac15b40b013b
shining8543 <shining8543@naver.com>
2021-08-13 16:11:54 +0900

    feat:[#753] plan DTO & Repo & SQL

---
+++ backend/danim-back/src/main/java/com/pd/danim/DTO/Plan.java
@@ -0,0 +1,73 @@
+package com.pd.danim.DTO;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@DynamicInsert
+@EntityListeners(AuditingEntityListener.class)
+public class Plan {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="plan_no")
+	private long planNo;
+	
+	@ManyToOne
+	@JoinColumn(name="user_no")
+	private User user;
+	
+	@Column(name="start_date")
+	private LocalDateTime startDate;
+	
+	@Column(name="duration")
+	private int duration;
+
+	public long getPlanNo() {
+		return planNo;
+	}
+
+	public void setPlanNo(long planNo) {
+		this.planNo = planNo;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public LocalDateTime getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(LocalDateTime startDate) {
+		this.startDate = startDate;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+	
+	
+	
+	
+	
+}
