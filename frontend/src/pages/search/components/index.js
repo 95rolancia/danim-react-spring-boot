30f8fec7d85dc8f5832b253b18b5826e1cde3337
rolancia <95jjangjun@gmail.com>
2021-08-14 20:21:56 +0900

    feat: [#764] 검색 페이지 컴포넌트 들 묶어서 export

---
+++ frontend/src/pages/search/components/index.js
@@ -0,0 +1,6 @@
+export { default as AreaItem } from './area-item/area-item';
+export { default as AreaList } from './area-list/area-list';
+export { default as Content } from './content/content';
+export { default as Header } from './header/header';
+export { default as UserItem } from './user-item/user-item';
+export { default as UserList } from './user-list/user-list';
