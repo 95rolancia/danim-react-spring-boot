2819f1ff9ab42582a96976db6fa12a19a7372131
rolancia <95jjangjun@gmail.com>
2021-08-15 22:33:31 +0900

    refactor: [#796] user-list, user-item

--- frontend/src/pages/search/components/user-item/user-item.jsx
+++ frontend/src/pages/search/components/user-item/user-item.jsx
@@ -7,6 +7,7 @@ import {
   Avatar,
   Divider,
 } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   user: {
@@ -16,13 +17,25 @@ const useStyles = makeStyles((theme) => ({
 
 const UserItem = ({ user }) => {
   const classes = useStyles();
+  const history = useHistory();
+  const goToMyPage = () => {
+    history.push(`/main/${user.nickname}`);
+  };
+
   return (
     <>
-      <ListItem className={classes.user}>
+      <ListItem className={classes.user} onClick={goToMyPage}>
         <ListItemAvatar>
-          <Avatar src={user.img} />
+          <Avatar
+            src={
+              process.env.REACT_APP_IMAGE_BASE_URL +
+              user.nickname +
+              '/' +
+              user.profile
+            }
+          />
         </ListItemAvatar>
-        <ListItemText primary={user.name} />
+        <ListItemText primary={user.nickname} />
       </ListItem>
       <Divider variant="middle" />
     </>
