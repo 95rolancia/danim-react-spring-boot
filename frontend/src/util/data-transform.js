6bb6cfa527246edef4dffb2d85901c26cc9e62a6
rolancia <95jjangjun@gmail.com>
2021-08-15 22:35:01 +0900

    hotfix: [#795] date util library issue

--- frontend/src/util/data-transform.js
+++ frontend/src/util/data-transform.js
@@ -1,19 +1,21 @@
 export const getPlanDate = (date) => {
   if (date === undefined) return '정보 없음';
+  console.log(date.startDate.getDay());
   return (
     date.startDate.getFullYear() +
     '.' +
-    date.startDate.getMonth() +
+    (date.startDate.getMonth() + 1) +
     '.' +
-    date.startDate.getDay() +
+    date.startDate.getDate() +
     ' - ' +
-    date.endDate.getMonth() +
+    (date.endDate.getMonth() + 1) +
     '.' +
-    date.endDate.getDay()
+    date.endDate.getDate()
   );
 };
 
 export const getFullPlanDate = (date) => {
-  const days = date.endDate.getDay() - date.startDate.getDay() + 1;
-  return days;
+  const diffTime = Math.abs(date.endDate - date.startDate);
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  return diffDays + 1;
 };
