f1195b5f33b22270332deeb3b603481fd3a79295
rolancia <95jjangjun@gmail.com>
2021-08-15 22:28:24 +0900

    refactor: [#794] about search

--- frontend/src/pages/search/components/content/content.jsx
+++ frontend/src/pages/search/components/content/content.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
 import useSearch from '../../../../hooks/useSearch';
-import { AreaList, UserList } from '../';
+import { StoryList, UserList } from '../';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  content: {
+    textAlign: 'center',
+  },
+}));
 
 const Content = observer(() => {
   const search = useSearch();
-  return <>{search.searchType === 'User' ? <UserList /> : <AreaList />}</>;
+  const classes = useStyles();
+  return (
+    <section className={classes.content}>
+      {search.searchType === 'User' ? (
+        search.searchedUser.length > 0 ? (
+          <UserList />
+        ) : (
+          <h2>검색 결과가 없습니다.</h2>
+        )
+      ) : search.searchedStory.length > 0 ? (
+        <StoryList />
+      ) : (
+        <h2>검색 결과가 없습니다.</h2>
+      )}
+    </section>
+  );
 });
 
 export default Content;
--- frontend/src/pages/search/components/index.js
+++ frontend/src/pages/search/components/index.js
@@ -1,5 +1,5 @@
-export { default as AreaItem } from './area-item/area-item';
-export { default as AreaList } from './area-list/area-list';
+export { default as StoryItem } from './story-item/story-item';
+export { default as StoryList } from './story-list/story-list';
 export { default as Content } from './content/content';
 export { default as Header } from './header/header';
 export { default as UserItem } from './user-item/user-item';
