0344b4d4f7a8f242626c1cc1c1bface7fd7130d5
노영주 <at8in@naver.com>
2021-08-16 15:41:30 +0900

    feat: [#806] 게시글 디테일 좋아요, 댓글 component추가

--- frontend/src/pages/board-read/story-detail.jsx
+++ frontend/src/pages/board-read/story-detail.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
 import useStory from '../../hooks/useStory';
-import { BoardDetailMap } from '../../components/index';
-import DetailHeader from './components/detail-header';
-import DetailTab from './components/detail-tab';
+import {
+  DetailHeader,
+  BoardDetailMap,
+  DetailTab,
+  ReplyList,
+  LoveList,
+} from './components';
+import { Divider } from '@material-ui/core';
 
-const StoryDetail = observer(({ datas, no }) => {
+const StoryDetail = observer(({ datas, no, handleLike }) => {
   const story = useStory();
+
   return (
     <>
       <DetailHeader num={no} />
       <BoardDetailMap stories={datas} lat={story.lat} lng={story.lng} />
       <DetailTab datas={datas} nickname={datas.nickname} />
+      <Divider />
+      <LoveList
+        love={datas.isLove}
+        loveCnt={datas.loveCount}
+        no={no}
+        handleLike={handleLike}
+      />
+      <ReplyList />
     </>
   );
 });
