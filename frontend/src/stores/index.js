2a8e4acb55a9b99692fe2cdb61de380c93df5d46
muni <ansgml6491@naver.com>
2021-08-16 16:24:05 +0900

    refactor: [#225] store의 index.js 수정

--- frontend/src/stores/index.js
+++ frontend/src/stores/index.js
@@ -2,12 +2,10 @@ import React, { createContext } from 'react';
 import authStore from './auth-store';
 import userStore from './user-store';
 import searchStore from './search-store';
-import boardCreateStore from './board-create-store';
 
 export const AuthContext = createContext();
 export const UserContext = createContext();
 export const SearchContext = createContext();
-export const BoardCreateContext = createContext();
 
 const ProviderStores = ({ children }) => {
   return (
@@ -16,9 +14,6 @@ const ProviderStores = ({ children }) => {
         <SearchContext.Provider value={searchStore}>
           {children}
         </SearchContext.Provider>
-        <BoardCreateContext.Provider value={boardCreateStore}>
-          {children}
-        </BoardCreateContext.Provider>
       </UserContext.Provider>
     </AuthContext.Provider>
   );
