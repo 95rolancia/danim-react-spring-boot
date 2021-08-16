d4a41f7bf1a908d90377785092f31dc2fbe913bb
노영주 <at8in@naver.com>
2021-08-16 16:26:28 +0900

    refactor: home화면 메인헤더 추가

---
+++ frontend/src/pages/home/components/index.js
@@ -0,0 +1,3 @@
+export { default as HomeRoot } from '../components/home-root';
+export { default as HomePic } from '../components/home-pic';
+export { default as MainHeader } from '../../../components/header/header-main';
