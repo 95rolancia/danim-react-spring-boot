3132c98899cde4563a14a5e375f8ecf540ab3ff0
노영주 <at8in@naver.com>
2021-08-16 15:38:05 +0900

    feat: [#806] 게시물 좋아요 관련 http-story

--- frontend/src/service/http-story.js
+++ frontend/src/service/http-story.js
@@ -5,13 +5,46 @@ class HttpStory {
         this.instance = instance;
     }
 
-    async getStory(storyno) {
+    async getStory(storyNo) {
         try {
-            const res = await this.instance.get('/story/' + storyno);
+            const res = await this.instance.get('/story/' + storyNo);
             return res;
         } catch (error) {
             const res = error.response;
             throw new Error(`story get error ${(res, error)}`);
+        }
+    }
+
+    async like(storyNo) {
+        try {
+            const res = await this.instance.post('/love', storyNo);
+            return res;
+        } catch (error) {
+            const res = error.response;
+            if (res.status === 400) {
+                return res;
+            }
+
+            throw new Error(`Like Error ${error.response}`);
+
+        }
+    }
+
+    async unlike(storyNo) {
+        try {
+            const res = await this.instance.delete('/love', {
+                data: {
+                    storyNo: storyNo.storyNo,
+                },
+            });
+            return res;
+        } catch (error) {
+            const res = error.response;
+            console.log(res);
+            if (res.status === 400) {
+                return res;
+            }
+            throw new Error(`UnLike Error ${error.response}`);
         }
     }
 }
