92b64d62eafa946ab1a38a20f0b2e81f211112af
이종현 <dlwhdgus53@naver.com>
2021-08-17 13:51:06 +0900

    feat : [#791] fcm love-follow-comment

--- backend/danim-back/src/main/java/com/pd/danim/Service/NotificationService.java
+++ backend/danim-back/src/main/java/com/pd/danim/Service/NotificationService.java
@@ -14,6 +14,7 @@ import com.pd.danim.Form.Request.NotificationRequest;
 import com.pd.danim.Form.Request.NotificationSaveRequest;
 import com.pd.danim.Repository.UserRepository;
 
+//추후 util로 class명 변경
 @Service
 public class NotificationService {
 	
@@ -60,34 +61,59 @@ public class NotificationService {
 				notiRequest.getIsRead(), notiRequest.getType());
 
 		String toId = notiRequest.getToUserNickname();
-		System.out.println("팔로우 받은 사람의");
-		System.out.println(toId);
+		//	System.out.println("팔로우 받은 사람의 닉네임은" + toId);
 		String type = notiRequest.getType();
 
 
 		FirebaseDatabase database = FirebaseDatabase.getInstance();
 		DatabaseReference ref = database.getReference("noti"); // 최상위 root: noti
-//		ref.setValueAsync(nickname);
-//		ref.child(toId).setValueAsync(toId);
+		//	ref.setValueAsync(nickname);
+		//	ref.child(toId).setValueAsync(toId);
 
 		DatabaseReference notiRef = ref.child(toId); // 알림 받는 사람의 닉네임
 		DatabaseReference nextNotiRef = notiRef.push(); // 다음 키값으로 푸시
 		String postId = nextNotiRef.getKey(); // 현재 알람의 키값을 가져옴
 		DatabaseReference saveNoti = notiRef.child(postId);// to의 아이디 값의 child node
-//		saveNoti.setValueAsync(postId);
+
+		//	saveNoti.setValueAsync(postId);		
+		//	User user = userRepository.findByNickname(nickname);
+		//	if(user.getProfile()!=null) {
+		//		saveRequest.setProfile(user.getProfile());
+		//	}
 		
-//		User user = userRepository.findByNickname(nickname);
-//		if(user.getProfile()!=null) {
-//			saveRequest.setProfile(user.getProfile());
-//		}
-		
-		
+		//다르게 설정해줘야함
 		if(type.equals("follow")) {
-			System.out.println("follow 요청 성공");
+			saveNoti.setValueAsync(saveRequest);
+		} else if(type.equals("love")){
+			saveNoti.setValueAsync(saveRequest);
+		} else {
 			saveNoti.setValueAsync(saveRequest);
 		}
-		
-
 	}
+	
+	
+	@Transactional
+	public void delteNoti(String notiId, String nickname) {
+		
+		FirebaseDatabase database = FirebaseDatabase.getInstance();
+		DatabaseReference ref = database.getReference("noti");
+		DatabaseReference notiRef = ref.child(notiId);
+		
+//		System.out.println(notiRef);
+//		System.out.println(notiRef.getRef());
+		
+		DatabaseReference deleteRef = notiRef.child(notiId);
+		deleteRef.removeValueAsync();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
