5f39545c43a4051d618eaecc5745a917e42cbb39
rolancia <95jjangjun@gmail.com>
2021-08-17 22:40:56 +0900

    feat: [#968] 검색 결과 디자인

--- frontend/src/pages/plan-search/components/content/content.jsx
+++ frontend/src/pages/plan-search/components/content/content.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import { observer } from 'mobx-react-lite';
 import useSearch from '../../../../hooks/useSearch';
 import { PlaceList } from '../';
-import { makeStyles } from '@material-ui/core';
+import { makeStyles, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   content: {
     textAlign: 'center',
+  },
+  searchResult: {
+    paddingTop: theme.spacing(6),
+  },
+  searchText: {
+    fontFamily: 'MingukBold',
+    color: '#36434C',
   },
 }));
 
@@ -18,7 +25,21 @@ const Content = observer(() => {
       {search.searchedPlace.length > 0 ? (
         <PlaceList />
       ) : (
-        <h2>검색 결과가 없습니다.</h2>
+        <section className={classes.searchResult}>
+          <img
+            src="/images/danilogo.png"
+            alt="logo"
+            width="70em"
+            height="50em"
+          ></img>
+          <Typography
+            variant="h5"
+            component="h5"
+            className={classes.searchText}
+          >
+            검색 결과가 없습니다.
+          </Typography>
+        </section>
       )}
     </section>
   );
