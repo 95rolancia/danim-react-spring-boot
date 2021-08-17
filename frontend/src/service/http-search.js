fb19b2f556aeee365fc3f4fa93391df332c538f9
rolancia <95jjangjun@gmail.com>
2021-08-17 11:29:57 +0900

    feat: [#946] search-store about plan

--- frontend/src/service/http-search.js
+++ frontend/src/service/http-search.js
@@ -30,6 +30,32 @@ class HttpSearch {
       throw new Error(`search nickname error ${(res, error)}`);
     }
   }
+
+  async searchPlace(keyword) {
+    try {
+      const res = await this.instance.get(`/plan/place/${keyword}`);
+      return res;
+    } catch (error) {
+      const res = error.response;
+      if (res.status === 404) {
+        return res;
+      }
+      throw new Error(`search place error ${(res, error)}`);
+    }
+  }
+
+  async getPlaceInfo(keyword) {
+    try {
+      const res = await this.instance.get(`/plan/place/detail/${keyword}`);
+      return res;
+    } catch (error) {
+      const res = error.response;
+      if (res.status === 404) {
+        return res;
+      }
+      throw new Error(`search place error ${(res, error)}`);
+    }
+  }
 }
 
 export default new HttpSearch(instance);
--- frontend/src/stores/search-store.js
+++ frontend/src/stores/search-store.js
@@ -5,6 +5,8 @@ class SearchStore {
   searchType = 'Area';
   searchedStory = [];
   searchedUser = [];
+  searchedPlace = [];
+  placeDetailInfo = null;
 
   constructor() {
     makeAutoObservable(this);
@@ -33,6 +35,33 @@ class SearchStore {
     runInAction(() => {
       this.searchedUser = res.data;
       this.searchType = 'User';
+    });
+  }
+
+  async searchPlace(keyword) {
+    if (!keyword) return;
+    const res = await HttpSearch.searchPlace(keyword);
+    console.log(res);
+    if (res.status !== 200) {
+      return;
+    }
+
+    runInAction(() => {
+      this.searchedPlace = res.data.places;
+    });
+  }
+
+  async getPlaceInfo(keyword) {
+    if (!keyword) return;
+    const res = await HttpSearch.getPlaceInfo(keyword);
+    console.log(res);
+    if (res.status !== 200) {
+      return;
+    }
+
+    runInAction(() => {
+      this.placeDetailInfo = res.data;
+      console.log(this.placeDetailInfo);
     });
   }
 }
