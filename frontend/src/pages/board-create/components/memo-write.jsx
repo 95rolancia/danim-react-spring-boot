e34888cb6de3d18dec94fcedfa0b7b1b99109c79
muni <ansgml6491@naver.com>
2021-08-19 11:29:19 +0900

    refactor: [#941] 스토리 수정

---
+++ frontend/src/pages/board-create/components/memo-write.jsx
@@ -0,0 +1,195 @@
+import React, { useState } from 'react';
+import { toJS } from 'mobx';
+import { observer } from 'mobx-react-lite';
+import { useHistory } from 'react-router-dom';
+import useBoardCreate from '../../../hooks/useBoardCreate';
+import { StoryCover, StoryContents } from './index';
+import {
+  makeStyles,
+  Button,
+  Grid,
+  FormGroup,
+  FormControlLabel,
+  Checkbox,
+  Snackbar,
+} from '@material-ui/core';
+
+import MuiAlert from '@material-ui/lab/Alert';
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    bottom: theme.spacing(0),
+    marginTop: theme.spacing(1),
+    padding: theme.spacing(1.5),
+    borderRadius: '30px',
+  },
+  buttonTextWhite: {
+    bottom: theme.spacing(0),
+    marginTop: theme.spacing(1),
+    padding: theme.spacing(1.5),
+    borderRadius: '30px',
+    color: 'white',
+  },
+}));
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+const MemoWrite = observer(({ onFileChange }) => {
+  const history = useHistory();
+  const boardCreate = useBoardCreate();
+  const classes = useStyles();
+  const [publishStatus, setPublishedStatus] = useState('PUBLISHED');
+  const [snackbarInfo, setSnackbarInfo] = useState({
+    isShow: false,
+    msg: '',
+    state: '',
+  });
+
+  const handleSubmitStory = () => {
+    const obj = {
+      duration: boardCreate.tripDate.length,
+      photos: toJS(boardCreate.photos),
+      startDate: toJS(boardCreate.photos[0].date),
+      status: publishStatus,
+      thumbnail: boardCreate.thumbnail,
+      title: boardCreate.title,
+    };
+
+    switch (boardCreate.status) {
+      case 'TEMP':
+        boardCreate.updateStory(obj).then((res) => {
+          if (res) {
+            history.push(`/main/${boardCreate.nickname}`);
+          } else {
+            setSnackbarInfo({
+              isShow: true,
+              msg: '스토리 작성에 오류가 발생했습니다.',
+              state: 'error',
+            });
+          }
+        });
+        break;
+      case '':
+        boardCreate.setStory(obj).then((res) => {
+          if (res) {
+            history.push('/read/' + res.data);
+          } else {
+            setSnackbarInfo({
+              isShow: true,
+              msg: '스토리 작성에 오류가 발생했습니다.',
+              state: 'error',
+            });
+          }
+        });
+
+        break;
+      default:
+        throw new Error(`unknown status ${boardCreate.status}`);
+    }
+    boardCreate.reset();
+  };
+
+  const handleSaveStory = () => {
+    const obj = {
+      duration: toJS(boardCreate.tripDate.length),
+      photos: toJS(boardCreate.photos),
+      startDate: toJS(boardCreate.photos[0].date),
+      status: 'TEMP',
+      thumbnail: toJS(boardCreate.thumbnail),
+      title: toJS(boardCreate.title),
+    };
+
+    boardCreate.setStory(obj).then((res) => {
+      if (res) {
+        console.log(res);
+        boardCreate.reset();
+        history.push('/read/' + res.data);
+      } else {
+        setSnackbarInfo({
+          isShow: true,
+          msg: '스토리 임시 저장에 오류가 발생했습니다.',
+          state: 'error',
+        });
+      }
+    });
+  };
+
+  const handleChecked = (e) => {
+    const value = e.target.checked;
+    boardCreate.handleIsTempChecked(value);
+    if (e.target.checked) {
+      setPublishedStatus('PRIVATED');
+    }
+  };
+
+  const handleClose = (_, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setSnackbarInfo({
+      ...snackbarInfo,
+      isShow: false,
+    });
+  };
+
+  return (
+    <>
+      <StoryCover />
+      <StoryContents />
+      <Grid container spacing={1}>
+        <Grid item xs={12}>
+          <FormGroup>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={boardCreate.isTempChecked}
+                  onChange={handleChecked}
+                  color="primary"
+                />
+              }
+              label="일기 공개하기"
+            />
+          </FormGroup>
+        </Grid>
+        <Grid item xs={4}>
+          <Button
+            fullWidth
+            variant="outlined"
+            color="primary"
+            component="span"
+            className={classes.button}
+            onClick={handleSaveStory}
+          >
+            임시저장
+          </Button>
+        </Grid>
+        <Grid item xs={8}>
+          <Button
+            fullWidth
+            variant="contained"
+            color="primary"
+            component="span"
+            className={classes.buttonTextWhite}
+            onClick={handleSubmitStory}
+          >
+            등록완료
+          </Button>
+        </Grid>
+        <Grid item></Grid>
+      </Grid>
+      <Snackbar
+        open={snackbarInfo.isShow}
+        autoHideDuration={700}
+        onClose={handleClose}
+      >
+        <Alert onClose={handleClose} severity={snackbarInfo.state}>
+          {snackbarInfo.msg}
+        </Alert>
+      </Snackbar>
+    </>
+  );
+});
+
+export default MemoWrite;
---
+++ frontend/src/pages/board-create/components/title-create.jsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState, useRef } from 'react';
+import useBoardCreate from '../../../hooks/useBoardCreate';
+import { observer } from 'mobx-react-lite';
+import { makeStyles, Typography, TextField, Fab, Box } from '@material-ui/core';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+
+const useStyles = makeStyles((theme) => ({
+  titleBox: {
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
+  },
+  titleInput: {
+    marginTop: theme.spacing(2),
+  },
+  button: {
+    right: 0,
+    bottom: theme.spacing(0),
+    marginTop: theme.spacing(3),
+    padding: theme.spacing(1.5),
+    borderRadius: '30px',
+  },
+}));
+
+const TitleCreate = observer(({ onFileChange }) => {
+  const titleRef = useRef();
+  const boardCreate = useBoardCreate();
+  const classes = useStyles();
+  const [defaultTitle, setDefaultTitle] = useState();
+
+  useEffect(() => {
+    const makeDefaultTitle = () => {
+      let today = new Date();
+      let year = today.getFullYear();
+      let month = ('0' + (today.getMonth() + 1)).slice(-2);
+      let day = ('0' + today.getDate()).slice(-2);
+
+      return year + '-' + month + '-' + day + '의 일기';
+    };
+    setDefaultTitle(makeDefaultTitle());
+  }, []);
+
+  const handleClick = (e) => {
+    boardCreate.handleTitleChange(titleRef.current.value);
+    boardCreate.handleLoading();
+    onFileChange(e);
+  };
+
+  return (
+    <form>
+      <div className={classes.titleBox}>
+        <Typography variant="h6" component="h6">
+          안녕하세요! {boardCreate.nickname}님!<br></br>
+          새로운 여행의 제목을 알려주세요.
+        </Typography>
+        <TextField
+          id="tripTitle"
+          placeholder={defaultTitle}
+          fullWidth
+          autoFocus
+          className={classes.titleInput}
+          inputRef={titleRef}
+        />
+      </div>
+      <Box display="flex" flexDirection="row-reverse">
+        <input
+          type="file"
+          id="input-file"
+          accept={'.jpg, .png'}
+          onChange={handleClick}
+          multiple
+          style={{ display: 'none' }}
+        />
+        <label htmlFor="input-file">
+          <Fab
+            variant="extended"
+            color="secondary"
+            component="span"
+            className={classes.button}
+          >
+            다음
+            <NavigateNextIcon />
+          </Fab>
+        </label>
+      </Box>
+    </form>
+  );
+});
+
+export default TitleCreate;
