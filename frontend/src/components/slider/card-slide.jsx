9e757a5c1dd64b60c506bbd3c1c3161ed884b89f
노영주 <at8in@naver.com>
2021-08-19 17:20:27 +0900

    style: 제목크기, 일자 수정

--- frontend/src/components/slider/card-slide.jsx
+++ frontend/src/components/slider/card-slide.jsx
@@ -1,13 +1,23 @@
 import { Typography } from '@material-ui/core';
 import React from 'react';
-
+import { makeStyles } from '@material-ui/styles';
+const useStyles = makeStyles((theme) => ({
+  title: {
+    fontFamily: 'MingukBold',
+    color: '#36434C',
+  },
+}));
 const CardSlide = ({ data }) => {
+  const classes = useStyles();
   return (
     <div>
-      <Typography variant="h5">{data.title}</Typography>
-      <Typography variant="body2">
-        총 {data.duration}일, {data.nickname}
+      <Typography className={classes.title} variant="h6">
+        {data.title}
       </Typography>
+      <Typography variant="body2">
+        {`${data.duration - 1}박 ${data.duration}일`}
+      </Typography>
+      <Typography variant="body2">{data.nickname}</Typography>
     </div>
   );
 };
