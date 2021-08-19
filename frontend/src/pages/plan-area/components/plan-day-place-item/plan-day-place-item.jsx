201de2c96d94d638b6a40b035dad3a85bd79c30b
rolancia <95jjangjun@gmail.com>
2021-08-19 14:30:47 +0900

    style: plan-day-place-item 꾸미기

--- frontend/src/pages/plan-area/components/plan-day-place-item/plan-day-place-item.jsx
+++ frontend/src/pages/plan-area/components/plan-day-place-item/plan-day-place-item.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
 import usePlan from '../../../../hooks/usePlan';
-import { makeStyles, ListItem, ListItemText, Divider } from '@material-ui/core';
+import {
+  makeStyles,
+  ListItem,
+  ListItemText,
+  Divider,
+  Typography,
+} from '@material-ui/core';
 import { Delete } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
   user: {
     cursor: 'pointer',
+  },
+  placeCnt: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: theme.spacing(0.2),
+    width: '1em',
+    height: '1em',
+    borderRadius: '50%',
+    color: 'white',
+    backgroundColor: 'darkgrey',
   },
 }));
 
@@ -21,7 +38,11 @@ const PlanDayPlaceItem = observer(({ place, order, day }) => {
   return (
     <>
       <ListItem className={classes.user}>
-        <ListItemText primary={order + 1} />
+        <ListItemText
+          primary={
+            <Typography className={classes.placeCnt}>{order + 1}</Typography>
+          }
+        />
         <ListItemText primary={place.name} />
         <Delete color="primary" onClick={removePlaceItem} />
       </ListItem>
